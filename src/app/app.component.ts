e90bc9a29be221b79547451fbb1398da15226af4
PallaviKatari <pallavikatari@gmail.com>
2022-02-22 12:14:31 +0530

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myFirstAngular';
+  title = 'I AM THE TITLE';
+  public image = "/assets/images/Angular1.jpg" //Property Binding
+  public random = "" //Two-way Binding
+  onClick(){  //Event Binding
+
+    console.log("I am Event Binding")
+
+  }
 }
